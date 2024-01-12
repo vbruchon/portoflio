d0c966f1da2f50ac36ddeba9fade6ce784fa00b9
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2024-01-12 14:32:00 +0100

    Create about section and "nav" for display about or resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { Header } from "./components/features/header/Header";
 import { Hero } from "./components/features/hero/Hero";
+import { MiniNav } from "./components/features/mininav/MiniNav";
 import { ProjectSection } from "./components/features/project/ProjectSection";
+import { Skills } from "./components/features/skills/Skills";
+import { Testimonial } from "./components/features/testimonial/Testimonial";
 import { ThemeProvider } from "./components/theme/ThemeProvider";
 
 function App() {
   return (
     <ThemeProvider>
       <Header />
-      <body>
-        <main className="mx-[5%] md:mx-[10%] xl:mx-[15%]">
-          <Hero />
-          <ProjectSection />
-        </main>
-      </body>
+      <main className="mx-[5%] md:mx-[10%] xl:mx-[15%]">
+        <Hero />
+        <MiniNav />
+        <Skills />
+        <ProjectSection />
+        {/* <Testimonial /> */}
+      </main>
     </ThemeProvider>
   );
 }
