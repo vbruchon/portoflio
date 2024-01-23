3575bd23de6562149a9b6ca1e6e709e6a09a6469
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2024-01-23 12:45:13 +0100

    update projects data and style

--- src/components/features/section/project/Project.tsx
+++ src/components/features/section/project/Project.tsx
@@ -1,54 +1,60 @@
-import { motion } from "framer-motion";
-import { CardContent } from "@/components/ui/card";
-import { ProjectLinks } from "./ProjectLinks";
-import { cn } from "@/lib/utils";
-import { useSectionIsVisible } from "@/hooks/useSectionIsVisible";
-import { ProjectType } from "./ProjectSection";
+import { motion } from 'framer-motion'
+import { CardContent } from '@/components/ui/card'
+import { ProjectLinks } from './ProjectLinks'
+import { cn } from '@/lib/utils'
+import { useSectionIsVisible } from '@/hooks/useSectionIsVisible'
+import { ProjectType } from './ProjectSection'
 
 type ProjectProps = {
-  project: ProjectType;
-  index: number;
-};
+    project: ProjectType
+    index: number
+}
 
 const item = {
-  hidden: { opacity: 0, y: 20 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.8,
-      ease: "easeInOut",
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.8,
+            ease: 'easeInOut',
+        },
     },
-  },
-};
+}
 
 export const Project = ({ project }: ProjectProps) => {
-  const controls = useSectionIsVisible("project-element");
+    const controls = useSectionIsVisible('project-element')
 
-  return (
-    <motion.div
-      id="project-element"
-      variants={item}
-      animate={controls}
-      initial="hidden"
-      className={cn(
-        "w-full h-full transition-all rounded-xl hover:scale-105 bg-card border relative",
-        "lg:w-1/3 lg:h-[455px] xl:h-[440px]"
-      )}
-    >
-      <img
-        src={project.thumbnailUrl}
-        alt={project.title}
-        className="object-cover w-full h-40 mb-4 rounded-xl"
-        style={{ borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }}
-      />
-      <CardContent className="flex flex-col flex-grow mt-auto">
-        <h3 className="mb-2 text-lg font-semibold text-foreground">
-          {project.title}
-        </h3>
-        <p className="mb-4 text-foreground">{project.description}</p>
-        <ProjectLinks githubUrl={project.githubUrl} demoUrl={project.demoUrl} />
-      </CardContent>
-    </motion.div>
-  );
-};
+    return (
+        <motion.div
+            id="project-element"
+            variants={item}
+            animate={controls}
+            initial="hidden"
+            className={cn(
+                'relative h-full w-full rounded-xl border bg-card transition-all hover:scale-105',
+                'lg:h-[585px] lg:w-1/3 xl:h-[480px]'
+            )}
+        >
+            <img
+                src={project.thumbnailUrl}
+                alt={project.title}
+                className="mb-4 h-48 w-full rounded-xl object-cover"
+                style={{
+                    borderBottomLeftRadius: 0,
+                    borderBottomRightRadius: 0,
+                }}
+            />
+            <CardContent className="mt-auto flex flex-grow flex-col">
+                <h3 className="mb-2 text-lg font-semibold text-foreground">
+                    {project.title}
+                </h3>
+                <p className="mb-4 text-foreground">{project.description}</p>
+                <ProjectLinks
+                    githubUrl={project.githubUrl}
+                    demoUrl={project.demoUrl}
+                />
+            </CardContent>
+        </motion.div>
+    )
+}
